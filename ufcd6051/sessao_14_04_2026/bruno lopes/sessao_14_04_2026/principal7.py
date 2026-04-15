517c973947e09c83226323ee2546e3e0393e5d0f
brunoirvilha <brunoirvilha@gmail.com>
2026-04-15 21:58:58 +0100

    Add files via upload

---
+++ ufcd6051/sessao_14_04_2026/bruno lopes/sessao_14_04_2026/principal7.py
@@ -0,0 +1,11 @@
+#ciclo principal
+while True:
+    #dados de entrada:
+    interrupetor_A = False
+    interrupetor_B = True
+    
+    #processamento
+    if(not interrupetor_A and interrupetor_B) or (interrupetor_A and not interrupetor_B):
+        ligar_luz = True
+    else:
+        ligar_luz = False
